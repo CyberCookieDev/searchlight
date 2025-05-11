5e3667e681e71884a87e38c02089c25397c91c4c
CyberCookie <olek.wiesiolek@gmail.com>
2025-05-11 16:09:25 +0200

    Initial commit
    Added basic functionality like calculating expressions, introduced basic command detection.

---
+++ main.py
@@ -0,0 +1,72 @@
+"""
+What is needed:
+- File search (basic)
+- File search by filetype
+- Google something - done
+- Calculations - done
+- Quick to-do
+"""
+import numexpr
+import webbrowser
+import urllib.parse
+import json
+
+def do_google_search(query):
+    q = urllib.parse.quote(query)
+    url = f"https://www.google.com/search?q={q}"
+    webbrowser.open(url)
+
+
+def calculate(expression):
+    result = numexpr.evaluate(expression)
+    print(f"= {result}")
+
+
+def save_tasks(tasks):
+    with open("tasks.json", "w") as f:
+        json.dump(tasks, f, indent=2)
+
+def load_tasks():
+     with open("tasks.json", "r") as f:
+        return json.load(f)
+     return []
+
+
+def show_tasks():
+    tasks = load_tasks()
+    x = 1
+    for task in tasks:
+        print(f"{x}. {task}")
+        x += 1
+
+def add_task(task):
+    tasks = load_tasks()
+    tasks.append(task)
+    save_tasks(tasks) 
+
+def delete_task(number):
+    tasks = load_tasks()
+    tasks.pop(number-1)
+
+
+def detect_command(data):
+    x = []
+    for y in data:
+        x.append(y)
+    if x[0] == "?":
+        x.reverse()
+        x.pop()
+        x.pop()
+        x.reverse()
+        x = "".join(x)
+        do_google_search(x)
+    if x[0] == "+":
+         x.reverse()
+         x.pop()
+         x.pop()
+         x.reverse()
+         x = "".join(x)
+         calculate(x)
+
+data = "+ 2 * 4 * (5+9) * (5-9)"
+detect_command(data)
